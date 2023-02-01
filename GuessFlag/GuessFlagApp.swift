06e9e37e83a89f565cb49fd8c4740c5cbc2670e2
Gökberk Ali Kulaç <gokberkkulac@gmail.com>
2023-02-01 12:40:10 +0300

    Initial Commit

---
+++ GuessFlag/GuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagApp.swift
+//  GuessFlag
+//
+//  Created by Gökberk Ali Kulaç on 1.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
